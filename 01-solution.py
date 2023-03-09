812e0ab14f51b5b54930407e6977f28e3f3f6436
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-09 09:40:26 -0800

    Uodate

--- 01-solution.py
+++ 01-solution.py
@@ -15,3 +15,13 @@ class Cell:
     def __init__(self, rels_value, visited):
         self.rels_value = rels_value
         self.visited = false
+
+
+if __name__ = "__main__":
+    # open input file and get all data
+
+
+    #do magice with data
+
+
+    # send data to output data
